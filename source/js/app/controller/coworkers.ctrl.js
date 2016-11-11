15c60d9b9d6745a35f69477307395bcd067a25a8
Rossana García Salirrosas <wendenlin13@hotmail.com>
2016-11-11 15:16:53 -0500

    Aplicar guía de estilos a Listado Colaboradores

--- source/js/app/controller/coworkers.ctrl.js
+++ source/js/app/controller/coworkers.ctrl.js
@@ -3,49 +3,45 @@
   angular.module('module.controller.coworkers', [])
     .controller('controller.coworkers', controllerCoworkers);
 
-    controllerCoworkers.$inject=[
-        '$scope',
-        '$resourceService',
-        '$state',
-        'employeeService',
-        'serviceStorage',
-        '$mdDialog',
-        '$mdToast',
-        '$q', '$timeout'
-    ];
+    controllerCoworkers.$inject=['$scope','$resourceService','$state','employeeService','serviceStorage','$mdDialog','$mdToast','$q', '$timeout'];
 
     function controllerCoworkers($scope,$resourceService,$state,coworkersService,serviceStorage,$mdDialog,$mdToast,$q, $timeout) {
 
-      var listEmployee=function(employee){
-        var objReq={};
-        objReq.search=employee;
-        coworkersService.empĺoyee.list(objReq,function (response) {
-          var array_users=[];
-          for(var i in response.results){
-            var detail_user=response.results[i];
-            array_users.push(detail_user);
-          }
+      /* private */
+      function list_Employee(employee){
+          var objReq={};
+          objReq.search=employee;
           $scope.active="Activo";
           $scope.selected = null;
           $scope.searchText = '';
-          $scope.users = array_users;
-          $scope.selected = $scope.users[0];
-          if(employee!=null){
-            $scope.searchText=employee;  
-          }
-          stopWaitingEffect();
-        }, function (error) {
-            showError(error);
-        });
+
+          coworkersService.empĺoyee.list(objReq,function (response) {
+            var array_users=[];
+            for(var i in response.results){
+              var detail_user=response.results[i];
+              array_users.push(detail_user);
+            }
+
+            $scope.users = array_users;
+            $scope.selected = $scope.users[0];
+            if(employee!=null){
+              $scope.searchText=employee;
+            }
+            stopWaitingEffect();
+          }, function (error) {
+              showError(error);
+          });
       }
 
+      /* public */
       $scope.list_specific=function(employee){
-        listEmployee(employee);
+        list_Employee(employee);
       }
 
-      listEmployee(null);
+      list_Employee(null);
 
-      var onChange=function(user){
+      /* private */
+      function onChange(user){
         coworkersService.empĺoyee.updateBlock({employee_id : user.pk,action : user.is_blocked},function (response) {
         stopWaitingEffect();
         showSimpleToast('EXITO. Se actualizó el registro correctamente');
@@ -55,11 +51,11 @@
           }else{
             user.is_blocked=true;
           }
-          
+
           showError(error);
         });
       }
-
+      /* public */
       $scope.showConfirm = function(ev,user) {
         var confirm = $mdDialog.confirm()
               .title('Confirmación')
@@ -76,30 +72,33 @@
             user.is_blocked=true;
           }
         });
-      }; 
+      };
 
+      /* public */
       $scope.selectUser = function (user) {
-        console.log("User selected : "+user.pk);
         $state.go('coworker-detail', {employee_id: user.pk})
       };
 
-      var showSimpleToast = function(messages) {
+      /* private */
+      function showSimpleToast(messages){
         $mdToast.show(
           $mdToast.simple()
             .textContent(messages)
             .position('bottom right' )
             .hideDelay(3000)
         );
-      };
+      }
 
-      var stopWaitingEffect=function(){
+       /* private */
+      function stopWaitingEffect(){
         $scope.loading=false;
       }
-      
-      var showError=function(error){
+
+       /* private */
+      function showError(error){
         showSimpleToast("ERROR EN EL PROCESO. Status : "+error.status+", "+error.statusText);
         $scope.loading=false;
       }
-      
+
     }
 })();
