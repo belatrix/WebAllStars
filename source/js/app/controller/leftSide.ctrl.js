e0087bb66211c05464389b341fa128daa0f225c3
danielisaacgeslin <danielisaacgeslin@gmail.com>
2016-11-15 16:03:14 -0300

    leftSide.ctrl

--- source/js/app/controller/leftSide.ctrl.js
+++ source/js/app/controller/leftSide.ctrl.js
@@ -1,24 +1,22 @@
 (function() {
-  'use strict';
-  angular.module('module.controller.leftSide', [])
-    .controller('controller.leftSide', controllerLeftSide); 
+    'use strict';
+    angular.module('module.controller.leftSide', [])
+        .controller('controller.leftSide', controllerLeftSide);
 
-    controllerLeftSide.$inject=[
-    	'$scope',
-    	'$mdSidenav',
-      '$mdUtil',
-      '$log'
-    ];
+    controllerLeftSide.$inject = ['$scope', '$mdSidenav', '$mdUtil', '$log'];
 
-    function controllerLeftSide($scope,$mdSidenav,$mdUtil,$log) {
+    function controllerLeftSide($scope, $mdSidenav, $mdUtil, $log) {
 
-      $scope.close = function () {
-        $mdSidenav('left').close()
-          .then(function () {
-            $log.debug("close LEFT is done");
-          });
-      };
-    	
+        $scope.close = close;
+
+        function close() {
+            $mdSidenav('left')
+                .close()
+                .then(function() {
+                    $log.debug("close LEFT is done");
+                });
+        }
+
     }
-    
+
 })();
