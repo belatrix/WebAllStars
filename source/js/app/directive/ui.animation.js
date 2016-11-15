c010f4115626233b4bed080e6a75ce25208d05db
Alexandro Estefano Castañeda Bello <u201522294@upc.edu.pe>
2016-11-15 16:21:24 -0500

    updating white spaces

--- source/js/app/directive/ui.animation.js
+++ source/js/app/directive/ui.animation.js
@@ -17,7 +17,7 @@
 
 			return directive;
 
-			function link($scope,element){
+			function link($scope, element){
 				var q = angular.element(element);
 
 				$rootScope.$on('$responseError',function(){
@@ -42,13 +42,13 @@
 
 			return directive;
 
-			function link($scope,element,$attr){
+			function link($scope, element, $attr) {
 				var q = angular.element(element);
 
-				if($attr.animatedSuccess){
-					$rootScope.$on('$responseSuccess',function(){
-						q.addClass('animated '+ $attr.animatedSuccess).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				    		q.removeClass('animated ' + $attr.animatedSuccess);
+				if($attr.animatedSuccess) {
+					$rootScope.$on('$responseSuccess', function() {
+						q.addClass('animated '+ $attr.animatedSuccess).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				    			q.removeClass('animated ' + $attr.animatedSuccess);
 			 			});
 					});
 				}
@@ -66,17 +66,17 @@
 
 			return directive;
 
-			function link($scope,element,$attr){
+			function link($scope, element, $attr) {
 
 				var q = angular.element(element);
 
-				if($attr.animatedLoad){
+				if($attr.animatedLoad) {
 					angular.element(document).ready(function () {
 						q.removeClass('hide');
-						q.addClass('animated '+ $attr.animatedLoad).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				    		q.removeClass('animated ' + $attr.animatedLoad);
+						q.addClass('animated '+ $attr.animatedLoad).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				    			q.removeClass('animated ' + $attr.animatedLoad);
 			 			});
-        			});
+        				});
 				}
 
 			}
@@ -92,15 +92,15 @@
 
 			return directive;
 
-			function link($scope,element){
+			function link($scope, element) {
 				var q = angular.element(element, $attr);
 
 				q.on('click',function(){
 
-					if($attr.animatedClick){
+					if($attr.animatedClick) {
 
-						q.addClass('animated '+ $attr.animatedClick).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				    		q.removeClass('animated ' + $attr.animatedClick);
+						q.addClass('animated '+ $attr.animatedClick).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				    			q.removeClass('animated ' + $attr.animatedClick);
 			 			});
 
 					}
