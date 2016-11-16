f100c8cfeb0a19d6f368b672f6e6c4cf42dd39d2
danielisaacgeslin <danielisaacgeslin@gmail.com>
2016-11-16 15:21:08 -0300

    start.service

--- source/js/app/service/start.service.js
+++ source/js/app/service/start.service.js
@@ -1,23 +1,18 @@
-
 (function() {
-	'use strict';
-	angular.module('module.service').factory('startService', startService);
+    'use strict';
+    angular.module('module.service').factory('startService', startService);
 
-	startService.$inject = ['serviceStorage','$resourceService','$state'];
+    startService.$inject = ['$state', '$resourceService', 'serviceStorage'];
 
-		function startService(serviceStorage, $resourceService, $state) {
+    function startService($state, $resourceService, serviceStorage) {
+        return {
+            list: list
+        };
 
-			return {
-				list: list
-			};
-
-            /** Get recommendations associated with a particular employee */
-			function list(query,fnSuccess,fnError) {
-				var startList = $resourceService.request('getStartsByEmployeeId');
-
-				return startList.get(query,fnSuccess,fnError);
-			}
-
-
-	}
+        /** Get recommendations associated with a particular employee */
+        function list(query, fnSuccess, fnError) {
+            var startList = $resourceService.request('getStartsByEmployeeId');
+            return startList.get(query, fnSuccess, fnError);
+        }
+    }
 })();
